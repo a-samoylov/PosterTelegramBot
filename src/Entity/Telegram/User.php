03fee65e39cbe7ee0ec26ade3caec4634f7878e4
egorgrushko <mailto.egorgrushko@gmail.com>
2018-10-20 22:43:49 +0300

    TelegramUser

---
+++ src/Entity/Telegram/User.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Telegram;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $phone;
+
+    /**
+     * @var \App\Entity\Telegram\Chat
+     * @ORM\OneToOne(targetEntity="App\Entity\Telegram\Chat")
+     * @JoinColumn(name="chat", nullable=true, referencedColumnName="id")
+     */
+    private $chat;
+
+    /**
+     * @var \App\Entity\Telegram\Bot
+     * @ORM\OneToOne(targetEntity="App\Entity\Telegram\Bot")
+     * @JoinColumn(name="telegram_bot", nullable=true, referencedColumnName="id")
+     */
+    private $telegramBot;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $currentLayout;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return \App\Entity\Telegram\Chat
+     */
+    public function getChat(): \App\Entity\Telegram\Chat
+    {
+        return $this->chat;
+    }
+
+    /**
+     * @param \App\Entity\Telegram\Chat $chat
+     */
+    public function setChat(\App\Entity\Telegram\Chat $chat): void
+    {
+        $this->chat = $chat;
+    }
+
+    public function getCurrentLayout(): int
+    {
+        return $this->currentLayout;
+    }
+
+    public function setCurrentLayout(int $currentLayout): void
+    {
+        $this->currentLayout = $currentLayout;
+    }
+
+    public function getTelegramBot(): \App\Entity\Telegram\Bot
+    {
+        return $this->telegramBot;
+    }
+
+    public function setTelegramBot(\App\Entity\Telegram\Bot $telegramBot): void
+    {
+        $this->telegramBot = $telegramBot;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,28 +25,9 @@ class User
     private $login;
 
     /**
-     * @ORM\Column(type="string", length=50, nullable=true)
-     */
-    private $phone;
-
-    /**
      * @ORM\Column(type="string")
      */
     private $password;
-
-    /**
-     * @var \App\Entity\Telegram\Chat
-     * @ORM\OneToOne(targetEntity="App\Entity\Telegram\Chat")
-     * @JoinColumn(name="chat", nullable=true, referencedColumnName="id")
-     */
-    private $chat;
-
-    /**
-     * @var \App\Entity\Telegram\Bot
-     * @ORM\OneToOne(targetEntity="App\Entity\Telegram\Bot")
-     * @JoinColumn(name="telegram_bot", nullable=true, referencedColumnName="id")
-     */
-    private $telegramBot;
 
     /**
      * @var string|null
@@ -59,11 +40,6 @@ class User
      * @ORM\Column(type="string", nullable=true)
      */
     private $posterAccessKey;
-
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $currentLayout;
 
     /**
      * @ORM\Column(type="text", nullable=true)
@@ -128,54 +104,6 @@ class User
     }
 
     /**
-     * @return string
-     */
-    public function getPhone(): string
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @param string $phone
-     */
-    public function setPhone(string $phone): void
-    {
-        $this->phone = $phone;
-    }
-
-    /**
-     * @return \App\Entity\Telegram\Chat
-     */
-    public function getChat(): \App\Entity\Telegram\Chat
-    {
-        return $this->chat;
-    }
-
-    /**
-     * @param \App\Entity\Telegram\Chat $chat
-     */
-    public function setChat(\App\Entity\Telegram\Chat $chat): void
-    {
-        $this->chat = $chat;
-    }
-
-    /**
-     * @return \App\Entity\Telegram\Bot
-     */
-    public function getTelegramBot(): \App\Entity\Telegram\Bot
-    {
-        return $this->telegramBot;
-    }
-
-    /**
-     * @param \App\Entity\Telegram\Bot $telegramBot
-     */
-    public function setTelegramBot(\App\Entity\Telegram\Bot $telegramBot): void
-    {
-        $this->telegramBot = $telegramBot;
-    }
-
-    /**
      * @return string|null
      */
     public function getPosterAccount(): ?string
@@ -205,22 +133,6 @@ class User
     public function setPosterAccessKey(?string $posterAccessKey): void
     {
         $this->posterAccessKey = $posterAccessKey;
-    }
-
-    /**
-     * @return int
-     */
-    public function getCurrentLayout(): int
-    {
-        return $this->currentLayout;
-    }
-
-    /**
-     * @param int $currentLayout
-     */
-    public function setCurrentLayout(int $currentLayout): void
-    {
-        $this->currentLayout = $currentLayout;
     }
 
     public function getPosterMenu(): \App\Entity\User\Poster\Menu
