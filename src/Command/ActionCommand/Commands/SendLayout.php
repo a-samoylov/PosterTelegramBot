0786d4059aec2d6ba821e225fe04f1bf14378638
Alexander Samoylov <a.samoylovua@gmail.com>
2018-10-21 15:30:09 +0300

    send layout

--- src/Command/ActionCommand/Commands/SendLayout.php
+++ src/Command/ActionCommand/Commands/SendLayout.php
@@ -10,6 +10,12 @@ namespace App\Command\ActionCommand\Commands;
 
 class SendLayout extends \App\Command\ActionCommand\BaseAbstract
 {
+
+    /**
+     * @var \App\Repository\Telegram\LayoutRepository
+     */
+    private $layoutRepository;
+
     /**
      * @var \App\Telegram\Model\Methods\Send\Message\Factory
      */
@@ -28,10 +34,12 @@ class SendLayout extends \App\Command\ActionCommand\BaseAbstract
     // ########################################
 
     public function __construct(
+        \App\Repository\Telegram\LayoutRepository                                              $layoutRepository,
         \App\Telegram\Model\Methods\Send\Message\Factory                                       $sendMessageFactory,
         \App\Telegram\Model\Type\ReplyMarkup\InlineKeyboardMarkup\Factory                      $inlineKeyboardMarkupFactory,
         \App\Telegram\Model\Type\ReplyMarkup\InlineKeyboardMarkup\InlineKeyboardButton\Factory $inlineKeyboardButtonFactory
     ) {
+        $this->layoutRepository            = $layoutRepository;
         $this->sendMessageFactory          = $sendMessageFactory;
         $this->inlineKeyboardMarkupFactory = $inlineKeyboardMarkupFactory;
         $this->inlineKeyboardButtonFactory = $inlineKeyboardButtonFactory;
@@ -46,6 +54,10 @@ class SendLayout extends \App\Command\ActionCommand\BaseAbstract
      */
     public function validate(array $params)
     {
+        if (!isset($params['layout_id'])) {
+            return 'Invalid param layout_id';
+        }
+
         return true;
     }
 
@@ -53,8 +65,35 @@ class SendLayout extends \App\Command\ActionCommand\BaseAbstract
 
     public function processCommand(\App\Entity\Telegram\User $user, array $params): void
     {
-        $a = 2;
-        // TODO: Implement processCommand() method.
+        $layout = $this->layoutRepository->findOneBy(['layoutId' => $params['layout_id']]);
+        if (is_null($layout)) {
+            return;
+        }
+
+        $sendMessageModel = $this->sendMessageFactory->create($user, $layout->getBot(), $layout->getText());
+
+        $replyMarkup = $layout->getReplyMarkup();
+        if (!empty($replyMarkup)) {
+            if ($replyMarkup['type'] === \App\Telegram\Bot\BotGenerator\Settings::TYPE_REPLY_INLINE_KEYBOARD_MARKUP) {
+                $inlineKeyboardMarkup = $this->inlineKeyboardMarkupFactory->create();
+
+                foreach ($replyMarkup['buttons'] as $buttonsRow) {
+                    $row = [];
+                    foreach ($buttonsRow as $inlineButton) {
+                        $row[] = $this->inlineKeyboardButtonFactory->create($inlineButton['text'], json_encode([
+                            'lt'  => $layout->getId(),
+                            'btn' => $inlineButton['id']
+                        ]));
+                    }
+
+                    $inlineKeyboardMarkup->addRowInlineKeyboard($row);
+                }
+
+                $sendMessageModel->setReplyMarkup($inlineKeyboardMarkup);
+            }
+        }
+
+        $sendMessageModel->send();
     }
 
     // ########################################
